c60633af1a027c1e1df9035f50874ba69d65ecf7
emhw0930 <123013521+emhw0930@users.noreply.github.com>
2026-07-17 20:23:14 -0400

    Add: ransom-note - submission-1

---
+++ Data Structures & Algorithms/ransom-note/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = [0] * 26
+        for i in range(len(magazine)):
+            count[ord(magazine[i]) - ord('a')] += 1
+        for i in range(len(ransomNote)):
+            count[ord(ransomNote[i]) - ord('a')] -= 1
+            if count[ord(ransomNote[i]) - ord('a')] < 0:
+                return False
+        return True
+        
