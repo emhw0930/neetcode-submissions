3ffc0bd633f400b99a4db661966734ff7e708e80
emhw0930 <123013521+emhw0930@users.noreply.github.com>
2026-07-31 15:48:51 -0400

    Add: longest-palindrome - submission-0

---
+++ Data Structures & Algorithms/longest-palindrome/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counter = Counter(s)
+        odd = False
+        result = 0
+        print(counter)
+        for k, v in counter.items():
+            if v % 2 == 1:
+                odd = True
+            result += v // 2 * 2
+        if odd:
+            result += 1
+        return result
+        
