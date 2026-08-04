3bd06bd1605f1f0012de3b26cbe5ef1bb23e21ec
emhw0930 <123013521+emhw0930@users.noreply.github.com>
2026-08-04 02:09:23 -0400

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # make a map {val: index (last appear)}
+        # for num in nums:
+        #   if num in map: find the difference, 
+        #       if the difference <= k reutrn True, 
+        #       otherwise update the last appearance
+        mapi = {}
+        for i, num in enumerate(nums):
+            if num in mapi:
+                if i - mapi[num] <= k:
+                    return True
+            mapi[num] = i
+        return False
+        
+        
