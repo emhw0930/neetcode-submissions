8032c9264677933b3bee3573c5dc245078030e9c
Ethan Wu <123013521+emhw0930@users.noreply.github.com>
2026-08-07 16:57:28 -0400

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        counting = Counter(nums)
+        sorting = sorted(counting.keys(), key=lambda x: (counting[x], -x))
+        result = []
+        for k in sorting:
+            result.extend([k] * counting[k])
+        return result
